3bb44acec2c6bab0018b0f0aaf01c5ae4925585a
Valerie Bayard <valerie.bayard@gmail.com>
2022-05-29 17:36:21 +0100

    changed structure to use 2 different pages for player turns

--- client/src/containers/CovatarsGameContainer.js
+++ client/src/containers/CovatarsGameContainer.js
@@ -1,6 +1,6 @@
 import react, { useState, useEffect } from 'react'
 import PlayerCreation from './PlayerCreation'
-import Play from './Play'
+import PlayerTurn from './PlayerTurn'
 
 const CovatarsGameContainer = () => {
 
@@ -9,15 +9,15 @@ const CovatarsGameContainer = () => {
             <h1> Covatars Game Container </h1>
             <p>Include routes to pages in here or in app.js to render following components as different pages:</p>
             <ul>
-                <li>PlayerCreation (= homepage?) </li>
+                <li>PlayerCreation (or = homepage?) </li>
                 <li>PlayerTurn for Player 1</li>
                 <li>PlayerTurn for Player 2</li>
-                <li>......... or Play page that will alternatively show PlayerTurn for player 1 or PlayerTurn for player 2</li>
             </ul>
             
             {/* DISPLAY TESTS - ALL WORKING */}
             {/* <PlayerCreation /> */}
-            {/* <Play/> */}
+            {/* <PlayerTurn player = "Player One" />
+            <PlayerTurn player = "Player Two" /> */}
         </>
     )
 }
--- client/src/containers/PlayerTurn.js
+++ client/src/containers/PlayerTurn.js
@@ -1,13 +1,13 @@
 import React from "react";
-import PlayerHand from "./PlayerHand";
-import PlayerBoard from "./PlayerBoard";
-import Deck from "./Deck";
+import PlayerHand from "../components/PlayerHand";
+import PlayerBoard from "../components/PlayerBoard";
+import Deck from "../components/Deck";
 
 const PlayerTurn = ({player}) => {
 
     return (
         <>
-            <h2> {player}'s' Turn</h2>
+            <h2> {player}'s Turn</h2>
             <Deck />
             <PlayerHand />
             <PlayerBoard />
