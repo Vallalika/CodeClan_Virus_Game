dcf881d5f6f35c98125154191413d83f1f23075c
Mario <magoro.graphics@gmail.com>
2022-05-30 14:53:54 +0100

    Adding test image to card

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -3,7 +3,10 @@ import React from "react";
 const Card = () => {
 
     return (
-        <h4> Card </h4>
+        <>
+            <h4> Card </h4>
+            <img src="./cards/red_organ.png" id="card-image" ></img>
+        </>
     )
 }
 
